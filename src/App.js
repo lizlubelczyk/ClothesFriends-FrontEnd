7bf4c0a13f28e25ee68cf7dc2804db69caa26aaa
Liz Lubelczyk <llubelczyk@mail.austral.edu.ar>
2024-04-18 17:28:39 -0300

    login signup funcionando

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Im
 import Login from './Components/Login-Signup/Login.jsx';
 import PasswordRecovery from './Components/Login-Signup/PasswordRecovery.jsx';
 import Signup from './Components/Login-Signup/Signup.jsx';
-
+import Feed from './Components/Home/Feed.jsx';
 function App() {
   return (
     <Router> {/* Use BrowserRouter */}
@@ -12,6 +12,7 @@ function App() {
         <Route path='/' element={<Login />} />
         <Route path='/Signup' element={<Signup />} />
         <Route path='/PasswordRecovery' element={<PasswordRecovery />} />
+        <Route path='/Feed' element={<Feed />} />
       </Routes>
     </Router>
   );
--- src/Components/Login-Signup/Login.jsx
+++ src/Components/Login-Signup/Login.jsx
@@ -9,6 +9,9 @@ import { useNavigate } from 'react-router-dom';
 function Login() {
 
   const navigate = useNavigate(); // Initialize useHistory
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [message, setMessage] = useState('');
 
   const handleSignUpClick = () => {
     // Navigate to the Sign-Up screen
@@ -19,14 +22,34 @@ function Login() {
     // Navigate to the Password Recovery screen
     navigate('/PasswordRecovery'); // Replace '/password-recovery' with your actual route
   };
+
+  const handleLogin = async (event) => {
+    event.preventDefault();
+  
+    const response = await fetch('http://localhost:8080/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ username, password }),
+    });
+  
+    const data = await response.json();
+  
+    if (response.ok) {
+      localStorage.setItem('token', data.token);
+      navigate('../Home/Feed'); // Redirect to Feed component
+    } else {
+      setMessage(data);
+    }
+  };
  
-  const [action, setAction] = useState("Iniciar Sesión ");
   return (
     <div className="container">
         <div className="circle">
         <img src={clothinghanger} alt="ClothesFriends Logo" className="logo" />
       </div>
-      <div className="bigsquare">
+      <div className="square">
       <div className="input">
             <img src={user} alt=""/>
             <input type="text" placeholder='Nombre de Usuario'/>
