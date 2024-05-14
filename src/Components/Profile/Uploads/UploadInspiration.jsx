77cfdd43b088d6ce7cb1946d6c7742138fc2f6d5
vmartinbotter <vmartinbotter@mail.austral.edu.ar>
2024-05-14 14:11:42 -0300

    subir foto en inspiracion

--- src/Components/Profile/Uploads/UploadInspiration.jsx
+++ src/Components/Profile/Uploads/UploadInspiration.jsx
@@ -7,6 +7,27 @@ import { FaTshirt, FaCamera, FaPen, FaHandHoldingHeart } from "react-icons/fa"; 
 
 
 function UploadInspiration(){
+    const [imagePreview, setImagePreview] = useState(null);
+    const [image, setImage] = useState(null); // Estado para la imagen seleccionada
+
+  // Función para manejar la selección de imagen
+  const handleImageUpload = (event) => {
+    const file = event.target.files[0]; // Get the selected image
+    setImage(file); // Update the selected image
+  
+    // Create a new FileReader object
+    const reader = new FileReader();
+  
+    // Set the imagePreview state when the file has been read
+    reader.onloadend = () => {
+      setImagePreview(reader.result);
+    };
+  
+    // Start reading the file as a DataURL
+    if (file) {
+      reader.readAsDataURL(file);
+    }
+  };
 
     return(
 
@@ -20,10 +41,11 @@ function UploadInspiration(){
                 <h1 className="title">Subir a mí perfil</h1>
             </div>
             <div className="square1">
+                <div className="image-upload-container">
                 <input
                 type="file"
                 className="image-upload-input" // Hidden input
-                //onChange={handleImageUpload}
+                onChange={handleImageUpload}
 
                 style={{ display: "none" }} // Keep the input hidden
                 />
@@ -35,18 +57,26 @@ function UploadInspiration(){
                 <FaCamera size={20} color="black" className="icon" />
                 Seleccionar Imagen
                 </button>
+                {imagePreview && (
+                    <img
+                    src={imagePreview}
+                    alt="Preview"
+                    className="image-preview"
+                    />
+                )}
+                </div>
 
 
-        <div className="input-descripción">
-          <FaPen size={20} className="icon" strokewidth={5} />
-          <textarea
-            className="field"
-            type="text"
-            placeholder="Descripción de la Prenda"
-            //value = {description}
-            //onChange={(e)=> setDescription(e.target.value)}
-          />
-        </div>
+                <div className="input-descripción">
+                <FaPen size={20} className="icon" strokewidth={5} />
+                <textarea
+                    className="field"
+                    type="text"
+                    placeholder="Descripción de la Prenda"
+                    //value = {description}
+                    //onChange={(e)=> setDescription(e.target.value)}
+                />
+                </div>
             </div>
         </div>
     )
